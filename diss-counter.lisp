adeef9bddc23e61a723db9dccaf94f118fe44f74
Raphael Santos <contact@raphaelss.com>
2015-05-25 18:37:29 -0300

    add license headers

--- diss-counter.lisp
+++ diss-counter.lisp
@@ -1,7 +1,23 @@
+;;; A dissonant counterpoint algorithm implementation in Common Lisp
+;;; Copyright (C) 2015 Raphael Santos, http://www.raphaelss.com
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU Affero General Public License as published
+;;; by the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU Affero General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU Affero General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (defpackage #:diss-counter
   (:use :cl)
-  (:export #:diss-counter #:next #:prob-fun #:reset-counts #:map-dc #:ref
-           #:size #:square-count #:prob-sum #:ref-count #:elem-count))
+  (:export #:diss-counter #:next #:prob-fun #:reset-counts #:map-dc #:ref #:size
+           #:square-count #:prob-sum #:ref-count #:elem-count))
 
 (in-package #:diss-counter)
 
